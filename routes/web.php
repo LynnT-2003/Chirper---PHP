103f4d112376a5e3d55834fc5ab42327d4e09263
Lynn <lnyinyi22@gmail.com>
2025-11-24 11:58:08 +0700

    Understand the Model-View-Controller pattern that keeps Laravel code organized. Create your first controller and see how MVC separates concerns in web applications.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+// this is bad practice, we should not return views directly from routes
+// instead set up controllers classes, and call the methods from there
+// this keeps everything strongly archetyped, clean, and modularized
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+// import Controller as Class, then pass in the Method (i.e. index, show, etc.)
+// we call this ChirpController the Class
+// and we call this index as the Method from that Controller Class
+Route::get('/', [ChirpController::class, 'index']);
 
 Route::get('/welcome', function () {
     return view('welcome');
